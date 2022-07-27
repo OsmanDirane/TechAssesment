181aafe41b2f7c22f015b5e58ed75649b602f1ae
Your Name <you@example.com>
2022-07-27 16:11:49 -0400

    finished

--- src/fanDriver.java
+++ src/fanDriver.java
@@ -1,16 +1,6 @@
 import java.util.Scanner;
 
-/**Filename: 
- * Author: Osman Dirane, 040976403
- * Course: CST8284 - OOP
- * Lab Section: 302
- * Assignment: TODO [number]
- * Date: TODO [the date of the final version]
- * Lab Professor: Sandra Iroakazi
- * Purpose: TODO [Brief description of the contents]
- * Class List: TODO [only if there is more than one class in the file]
- * 
- */
+
 
 /**
  * TODO [Brief Description of purpose of class]
